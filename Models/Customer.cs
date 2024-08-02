add492d67656a2138a31b7ce1d00d6c61ba7bdcc
Deilons <apolo4748@gmail.com>
2024-08-02 06:53:50 -0500

    Feat: Customer class propieties and methods

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,11 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace TransRiwi.Models;
 
-public class Customer
-{
+public class Customer : User
+{   
+    // properties
 
+    public string MembershipLevel { get; set; }
+
+    public string PreferredPaymentMethod { get; set; }
+
+    // constructor
+
+    public Customer(string name, string lastName, string typeOfDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string adress, string membershiplevel, string preferredpaymentMethod) : base(name, lastName, typeOfDocument, identificationNumber, birthDate, email, phoneNumber, adress)
+    { 
+        MembershipLevel = membershiplevel;
+        PreferredPaymentMethod = preferredpaymentMethod;
+    }
+
+    // methods
+
+    public void UpdateMembershipLevel(string NewMembershipLevel)
+    {
+        MembershipLevel = NewMembershipLevel;
+    }
 }
