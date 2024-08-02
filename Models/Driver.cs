09d6dbec569cdb1170bba966938bb304b1726324
Deilons <apolo4748@gmail.com>
2024-08-02 06:46:26 -0500

    Feat: Drivers class propieties and methods

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -5,7 +5,35 @@ using System.Threading.Tasks;
 
 namespace TransRiwi.Models;
 
-public class Driver
+public class Driver : User
 {
+    // properties
+
+    public string LicenseNumber { get; set; }
+
+    public string LicenseCategory { get; set; }
+
+    public int DrivingExperience { get; set; }
+
+    // constructor
+
+    public Driver(string name, string lastName, string typeOfDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string adress, string licenseNumber, string licenseCategory, int drivingExperience) : base(name, lastName, typeOfDocument, identificationNumber, birthDate, email, phoneNumber, adress)
+    {
+        LicenseNumber = licenseNumber;
+        LicenseCategory = licenseCategory;
+        DrivingExperience = drivingExperience;
+    }
+
+    // methods
+
+    public void UpdateLicenseCategory(string NewlicenseCategory)
+    {
+        LicenseCategory = NewlicenseCategory;
+    }
+
+    public void AddExperience( int Years)
+    {
+        DrivingExperience += Years;
+    }
 
 }
